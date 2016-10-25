618c2610b437277c51e88e8fc9ab5b1fc4c5254c
Matthew Stevenson <stevensonmt@gmail.com>
2016-10-24 17:42:34 -0700

    Add update function before creating form partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,27 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @user= User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      sign_in @user
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed"
+    redirect_to users_path
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
