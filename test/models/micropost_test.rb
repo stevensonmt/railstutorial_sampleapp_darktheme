c03ca76617f3d8a257c19e072add80e1e5befc1f
Matthew Stevenson <stevensonmt@gmail.com>
2016-11-04 20:22:13 -0700

    Complete 13.1

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -3,8 +3,7 @@ require 'test_helper'
 class MicropostTest < ActiveSupport::TestCase
   def setup
     @user = users(:michael)
-    # This code is not idiomatically correct.
-    @micropost = @user.microposts.new(content: "Lorem ipsum")
+    @micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
   test "should be valid" do
@@ -25,4 +24,10 @@ class MicropostTest < ActiveSupport::TestCase
     @micropost.content = "a" * 141
     assert_not @micropost.valid?
   end
+
+  test "order should be most recent first" do
+    assert_equal microposts(:most_recent), Micropost.first
+  end
+
+
 end
