60d0ff0bbcbf58ea2c38b78d67696b5f57c64ede
Matthew Stevenson <stevensonmt@gmail.com>
2016-11-05 08:42:26 -0700

    Complete 13.3.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
-  get 'sessions/new'
-
-  get 'users/new'
-
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
@@ -17,8 +9,20 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
+  get 'sessions/new'
+
+  get 'users/new'
+
+
+
+
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :microposts, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
