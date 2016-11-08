1b656928481d672d80ffeb53e607a682dfd62778
Matthew Stevenson <stevensonmt@gmail.com>
2016-11-07 20:55:39 -0800

    Complete 14.1.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token, :reset_token
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+    foreign_key: "follower_id",
+    dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
   before_save { self.email = email.downcase unless self.email.blank? }
   before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
@@ -75,7 +79,22 @@ class User < ApplicationRecord
    def feed
      Micropost.where("user_id = ?", id)
    end
-   
+
+   # Follows a user.
+   def follow(other_user)
+     active_relationships.create(followed_id: other_user.id)
+   end
+
+   # Unfollows a user.
+   def unfollow(other_user)
+     active_relationships.find_by(followed_id: other_user.id).destroy
+   end
+
+   # Returns true if the current user following the other user.
+   def following?(other_user)
+     following.include?(other_user)
+   end
+
    private
 
    #Converts email to all lower-case
